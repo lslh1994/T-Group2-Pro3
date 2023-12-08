78109650b9f58a5d369005204b3df67be526ca32
shin <nas221@naver.com>
2023-12-08 09:40:17 +0900

    2023.12.08 snr : 나눗셈 나머지

--- Project_03/src/com/lec/div/AddSubMulDiv.java
+++ Project_03/src/com/lec/div/AddSubMulDiv.java
@@ -4,6 +4,16 @@ import com.lec.mul.AddSubMul;
 
 public class AddSubMulDiv extends AddSubMul{
 	
+	public AddSubMulDiv() {
+		
+	}
+	
+	public AddSubMulDiv(int num1, int num2, int num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+
 	public int getDiv2(int num1, int num2) {
 		return num1/num2;
 	}
@@ -12,7 +22,13 @@ public class AddSubMulDiv extends AddSubMul{
 		return num1/num2/num3;
 	}
 	
-	public int getDiv4(int num1, int num2, int num3, int num4) {
-		return num1/num2/num3/num4;
+	public int getDiv3() {
+		return num1/num2/num3;
 	}
+	
+
+	public double getFDiv() {
+		return (double)num1/ num3 / num4;
+	}
+	
 }
--- Project_03/src/com/lec/remainder/AddSubMulDivRemainder.java
+++ Project_03/src/com/lec/remainder/AddSubMulDivRemainder.java
@@ -2,18 +2,18 @@ package com.lec.remainder;
 
 import com.lec.div.AddSubMulDiv;
 
-public class AddSubMulDivRemainder extends AddSubMulDiv{
+public class AddSubMulDivRemainder extends AddSubMulDiv {
 
-	public void getRemainder4(int num1, int num2, int num3, int num4) {
-		System.out.println(num1%num2%num3%(double)num4);
+	public AddSubMulDivRemainder() {
+		
 	}
 	
-	public void getRemainder3(int num1, int num2, int num3) {
-		System.out.println(num1%num2%(double)num3);
+	public int getRemainder2(int num1, int num2) {
+		return num1 % num2;
 	}
-	
-	public void getRemainder2(int num1, int num2) {
-		System.out.println(num1%num2);
+
+	public int getRemainder3(int num1, int num2, int num3) {
+		return num1 % num2 % num3;
 	}
-	
+
 }
